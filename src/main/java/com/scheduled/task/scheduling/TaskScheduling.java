17ffd51cea043732b87211b8da2df0d948c4b3a6
Izzatbek <abdusharipovizzat03@gmail.com>
2026-04-15 21:01:45 +0500

    Email message sent

--- src/main/java/com/scheduled/task/scheduling/TaskScheduling.java
+++ src/main/java/com/scheduled/task/scheduling/TaskScheduling.java
@@ -1,7 +1,9 @@
 package com.scheduled.task.scheduling;
 
+import com.scheduled.task.util.MessageUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +13,26 @@ import java.util.Date;
 
 @Component
 public class TaskScheduling {
+    @Value("${client.name}")
+    private String clientName;
     private static final Logger logger =  LoggerFactory.getLogger(TaskScheduling.class);
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
+    private final MessageUtil messageUtil;
+
+    public TaskScheduling(MessageUtil messageUtil){
+        this.messageUtil=messageUtil;
+    }
+
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime(){
-        logger.info("This is time now {}",dateFormat.format(new Date()));
+        logger.info("This is now {}",dateFormat.format(new Date()));
+    }
+
+
+    @Scheduled(fixedDelay = 5000)
+    public void sendMessage(){
+        messageUtil.sendMail(clientName,"Hello World","https://incridable-acuman.vercel.app/");
+        logger.info("Message successfully sent");
     }
 }
